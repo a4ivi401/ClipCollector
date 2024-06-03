d60c5edc8b29ba9cb5fba0d8cba167e1b656d115
a4ivi401 <belinskiy2007@gmail.com>
2024-06-03 13:48:56 +0300

    hotfix #1

--- handlers/update_handler.py
+++ handlers/update_handler.py
@@ -5,7 +5,7 @@ def register_handlers(bot):
     @bot.message_handler(commands=['info_update'])
     def info_update(message):
         if message.chat.id == config.ADMIN_CHAT_ID:
-            for i in open('chatids.txt', 'r').readlines():
+            for i in open('chat_ids.txt', 'r').readlines():
                 with open('update_list.txt', 'r') as file:
                     text = file.read()
                     bot.send_message(i, text)
